7623e782bb9fe51dba78ec72c828755934e54eb9
sebabrennan <brennan.seba@gmail.com>
2024-04-16 19:46:31 -0300

    realizando correciones

--- desafio1.js
+++ desafio1.js
@@ -13,13 +13,19 @@ class ProductManager {
             stock: stock,
             id: this.#initId()
         }
-        if(this.products.map((product)=> product ==! product.id), title, description, price, thumbnail, stock, code){
-            if (this.products.map((product)=> product === product.code)){
-                return console.log("No es posible agregar ese producto");
-            }
+        
+        if (title, description, price, thumbnail, code, stock) {            
+            const productsCheck = []
+            productsCheck.push(product)
+            const productsCheckSome = productsCheck.some((product)=>product.code === code)
+            //console.log(productsCheck.some((product)=>product.code === code));
+            if(productsCheckSome) return console.log("No es posible agregar ese producto")
         }else{
-            return this.products.push(product)
-        }}
+            return this.products.push(product);
+        }
+        //if(this.products.some((product)=>product.code ==! code))
+        //console.log(this.product.some((product)=>product.code ==! code));
+    }
 
     #initId(){
         let idCode = this.products.length + 1
@@ -31,7 +37,7 @@ class ProductManager {
     }
 
     getProductById(productId){
-        const checkId = this.products.find((product) => product.code === productId)
+        const checkId = this.products.find((product) => product.id === productId)
         if(!checkId) return "Not found"
         else return checkId
     }
@@ -40,7 +46,7 @@ class ProductManager {
 const productManager = new ProductManager()
 
 productManager.addProduct("REMERA HIGH PLEASURE", "Remera manga corta con cuello redondo", 29000, "https://acdn.mitiendanube.com/stores/001/343/102/products/tezza-90321-c88ac45352b76c5a7c16700049342456-640-0.webp,", 4, 1)
-productManager.addProduct("REMERA SMILE", "Fresca y cómoda para el verano, su tela permite que el pase del aire muy facilimente", 29000, "https://acdn.mitiendanube.com/stores/001/343/102/products/tezza-15361-b5442d7cbc68f9a81516700036167374-480-0.webp", 5, 3)
+productManager.addProduct("REMERA SMILE", "Fresca y cómoda para el verano, su tela permite que el pase del aire muy facilimente", 29000, "https://acdn.mitiendanube.com/stores/001/343/102/products/tezza-15361-b5442d7cbc68f9a81516700036167374-480-0.webp", 4, 3)
 productManager.addProduct()
 
 console.log(productManager.getProducts());
